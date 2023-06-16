e5547c49e64e26cd7cf374bf52663a7b947627cd
Yuya <e1c20056@oit.ac.jp>
2023-06-16 18:07:30 +0900

    Revert "repair"
    This reverts commit 018210177fa7bfe09fba74284e5b3892843e50a0.

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster GAME");
     System.out.println("Show a new monster!");
-    Player player = new Player();
-    player.drawMonsters();
-    player.showDeck();
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
 }
